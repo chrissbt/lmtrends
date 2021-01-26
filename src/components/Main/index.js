cf0e67dade2cdf9c6a2edbba342110da42cdac1c
krava-vlad <krava.vlad.kvn@gmail.com>
2021-01-26 07:51:15 -0800

    Fix: changing Custom Date Time picker

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -184,6 +184,11 @@ export default function Main() {
   const handleTimeChange = (event) => {
     event.preventDefault();
     selectShowTime(event.target.value);
+    if(event.target.value === 5){
+      selectShowCustomDate(true);
+    } else {
+      selectShowCustomDate(false);
+    }
   }
 
   const handleTagChange = (event) => {
@@ -191,92 +196,93 @@ export default function Main() {
     selectShowTag(event.target.value);
   }
 
+  const handleClose = () => {
+    selectShowCustomDate(false);
+  }
+
   useEffect(() => {
     let totalCount = 0;
     let newData = [];
     let count = 0;
-      selectShowCustomDate(false);
-      switch (showTime) {
-        case 0:
-          count = 0;
-          break;
-        case 1:
-          count = 90;
-          break;
-        case 2:
-          count = 60;
-          break;
-        case 3:
-          count = 30;
-          break;
-        case 4:
-          count = 14;
-          break;
-        case 5:
-          count = 100;
-          selectShowCustomDate(true);
-          break;
-        default:
-          break;
-      }
-
-      if(count === 0) {
-        totalCount = result.total;
-        newData = result.data;
-      } else if (count === 100){
-        newData = result.data.filter(({data}) => {
-          var dateObject = new Date(data['Date']);
-          var compare = dateObject.getTime() >= new Date(customDate[0]).getTime() && dateObject.getTime() <= new Date(customDate[1]).getTime();
-          if(!compare) {
-            return false;
-          }
-          return true;
-        }).map(function(tag) {
-          return tag;
-        });
-        totalCount = newData.length;
-      }
-      else {
-        let today = new Date()
-        let priorDate = new Date().setDate(today.getDate()-count);
-        priorDate = new Date(priorDate)
-        newData = result.data.filter(({data}) => {
-          var dateObject = new Date(data['Date']);
-          var compare = dateObject.getTime() >= priorDate.getTime();
-          if(!compare) {
-            return false;
-          }
-          return true;
-        }).map(function(tag) {
-          return tag;
-        });
-        totalCount = newData.length;
-      }
-      if (showTag === 0){
-        setResult((prev) =>({
-          ...prev,
-          totalCount,
-          newData
-        }));
-      } else {
-        newData = newData.filter(({data}) => {
-          var tagObject = data['Tag'];
-          tagObject = tagObject === 'No Tag' ? '' : tagObject;
-          var compare = tagObject === filterTags[showTag].value;
-          if(!compare) {
-            return false;
-          }
-          return true;
-        }).map(function(tag) {
-          return tag;
-        });
-        totalCount = newData.length;
-        setResult((prev) =>({
-          ...prev,
-          totalCount,
-          newData
-        }))
-      }
+    switch (showTime) {
+      case 0:
+        count = 0;
+        break;
+      case 1:
+        count = 90;
+        break;
+      case 2:
+        count = 60;
+        break;
+      case 3:
+        count = 30;
+        break;
+      case 4:
+        count = 14;
+        break;
+      case 5:
+        count = 100;
+        break;
+      default:
+        break;
+    }
+    if(count === 0) {
+      totalCount = result.total;
+      newData = result.data;
+    } else if (count === 100){
+      newData = result.data.filter(({data}) => {
+        var dateObject = new Date(data['Date']);
+        var compare = dateObject.getTime() >= new Date(customDate[0]).getTime() && dateObject.getTime() <= new Date(customDate[1]).getTime();
+        if(!compare) {
+          return false;
+        }
+        return true;
+      }).map(function(tag) {
+        return tag;
+      });
+      totalCount = newData.length;
+    }
+    else {
+      let today = new Date()
+      let priorDate = new Date().setDate(today.getDate()-count);
+      priorDate = new Date(priorDate)
+      newData = result.data.filter(({data}) => {
+        var dateObject = new Date(data['Date']);
+        var compare = dateObject.getTime() >= priorDate.getTime();
+        if(!compare) {
+          return false;
+        }
+        return true;
+      }).map(function(tag) {
+        return tag;
+      });
+      totalCount = newData.length;
+    }
+    if (showTag === 0){
+      setResult((prev) =>({
+        ...prev,
+        totalCount,
+        newData
+      }));
+    } else {
+      newData = newData.filter(({data}) => {
+        var tagObject = data['Tag'];
+        tagObject = tagObject === 'No Tag' ? '' : tagObject;
+        var compare = tagObject === filterTags[showTag].value;
+        if(!compare) {
+          return false;
+        }
+        return true;
+      }).map(function(tag) {
+        return tag;
+      });
+      totalCount = newData.length;
+      setResult((prev) =>({
+        ...prev,
+        totalCount,
+        newData
+      }))
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showTime, showTag])
 
@@ -318,6 +324,7 @@ export default function Main() {
             onChange={(newValue) => {
               setCustomDate(newValue);
             }}
+            onClose={handleClose}
             renderInput={(startProps, endProps) => (
               <React.Fragment>
                 <TextField {...startProps} variant="standard" />
@@ -331,11 +338,11 @@ export default function Main() {
       <Grid container spacing={3}>
         <Grid item>
           <TextField
-          id="time-currency"
-          select
-          value={showTime}
-          onChange={handleTimeChange}
-          disabled={result.total === 0}
+            id="time-filter"
+            select
+            value={showTime}
+            onChange={handleTimeChange}
+            disabled={result.total === 0}
           >
             {filterTime.map((time) => (
               <MenuItem key={time.value} value={time.id}>
@@ -346,11 +353,11 @@ export default function Main() {
         </Grid>
         <Grid item>
           <TextField
-          id="time-currency"
-          select
-          value={showTag}
-          onChange={handleTagChange}
-          disabled={result.total === 0}
+            id="tag-filter"
+            select
+            value={showTag}
+            onChange={handleTagChange}
+            disabled={result.total === 0}
           >
             {filterTags.map((tag) => (
               <MenuItem key={tag.value} value={tag.id}>
